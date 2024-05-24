d27f2a25047d932389f3256a64893500e22dac77
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-24 08:46:46 -0300

    feat: style StatusBar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { Loading } from '@components/Loading';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import Groups from '@screens/Groups';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
 import theme from 'src/theme';
 import { ThemeProvider } from 'styled-components';
 
@@ -12,6 +12,11 @@ export default function App() {
   })
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
 
