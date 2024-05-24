77c07ad320a7375270ecfdbe1ad967a7038fef0d
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-24 08:40:30 -0300

    feat: load roboto font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import Groups from '@screens/Groups';
+import { ActivityIndicator } from 'react-native';
 import theme from 'src/theme';
 import { ThemeProvider } from 'styled-components';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
 
   );
