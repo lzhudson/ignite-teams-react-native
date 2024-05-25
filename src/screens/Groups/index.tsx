e335b8b4c53c52f9a31d26d53526fd3950d49bbb
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-25 17:41:25 -0300

    feat: usage Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export default function Groups() {
   const [groups, setGroups] = useState<string[]>();
@@ -25,6 +26,7 @@ export default function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+      <Button title='Criar nova turma' />
     </Container>
   );
 }
