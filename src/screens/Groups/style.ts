4a1bdda69db5884aebbe0bd1135e564d43420eb3
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-25 16:32:57 -0300

    fix: remove items center

--- src/screens/Groups/style.ts
+++ src/screens/Groups/style.ts
@@ -3,7 +3,6 @@ import styled from "styled-components/native";
 export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-  align-items: center;
 `
 
 export const Title = styled.Text`
