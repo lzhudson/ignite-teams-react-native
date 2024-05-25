c100a80ea5aefe8a85fd14a0bc87374e83332318
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-25 18:20:53 -0300

    refact: render NewGroup

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { Loading } from '@components/Loading';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import Groups from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 import { ActivityIndicator, StatusBar } from 'react-native';
 import theme from 'src/theme';
 import { ThemeProvider } from 'styled-components';
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent" 
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
 
   );
