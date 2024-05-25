55165b72b9434cd5ad7d764f8764068a25b37090
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-25 16:32:48 -0300

    feat: use FlatList to render multiples GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,25 @@ import { Highlight } from '@components/Highlight';
 import { Container, Title } from './style';
 import { Header } from '@components/Header';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
+
   return (
     <Container>
       <Header />
 
       <Highlight title='Turmas' subtitle='Jogue com sua turma' />
       <GroupCard title='Galera do Ignite' />
+      <FlatList 
+        data={groups} 
+        keyExtractor={item => item} 
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
     </Container>
   );
 }
