9a04cb705360d5e2a2664f3a80ad665cd3cdcde9
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-25 16:22:42 -0300

    feat: usage GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { Highlight } from '@components/Highlight';
 import { Container, Title } from './style';
 import { Header } from '@components/Header';
+import { GroupCard } from '@components/GroupCard';
 
 export default function Groups() {
   return (
@@ -8,6 +9,7 @@ export default function Groups() {
       <Header />
 
       <Highlight title='Turmas' subtitle='Jogue com sua turma' />
+      <GroupCard title='Galera do Ignite' />
     </Container>
   );
 }
