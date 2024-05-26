af39608af3a3febcdba9e0770a1e0f835b1fbbb4
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-26 10:16:35 -0300

    feat: list filters and show number of players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,15 @@
 import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState(['Rodrigo'])
   return (
     <Container>
       <Header showBackButton />
@@ -21,8 +25,25 @@ export function Players() {
         />
         <ButtonIcon icon="add" />
       </Form>
+      <HeaderList>
+        <FlatList 
+          data={['Time A', 'Time B']} 
+          keyExtractor={item => item} 
+          renderItem={({ item }) => (
+            <Filter 
+              title={item} 
+              isActive={item === team} 
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
+      
 
-      <Filter title="Time A" isActive />
     </Container>
   )
 }
