854875d8a4b1c87f6b542931b40c2abf68779bc9
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-27 16:05:45 -0300

    feat: create function to navigate new group screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,9 +6,15 @@ import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>();
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <Container>
@@ -26,7 +32,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
-      <Button title='Criar nova turma' />
+      <Button title='Criar nova turma' onPress={handleNewGroup} />
     </Container>
   );
 }
