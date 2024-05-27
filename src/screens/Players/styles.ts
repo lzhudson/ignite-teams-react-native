67c321cd46f448b6d86bcdf7cad04dc8c3cc1604
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-27 16:30:49 -0300

    refact: change to SafeAreaView component

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,7 +1,7 @@
 import { css } from "styled-components";
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 
