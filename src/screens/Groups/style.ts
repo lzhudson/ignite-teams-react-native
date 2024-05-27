13cb19506afc3fd3fd5bfeb8e20639ccd645e0aa
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-27 16:38:13 -0300

    fix: add padding

--- src/screens/Groups/style.ts
+++ src/screens/Groups/style.ts
@@ -4,6 +4,7 @@ import styled from "styled-components/native";
 export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+  padding: 24px;
 `
 
 export const Title = styled.Text`
