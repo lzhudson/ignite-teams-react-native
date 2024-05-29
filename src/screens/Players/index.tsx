8ca6a4d30bed5073e37155b7d98749bc3523c19a
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-29 08:39:24 -0300

    feat: create function to add player in group

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -4,24 +4,52 @@ import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 import { useRoute } from "@react-navigation/native";
+import { AppError } from "src/utils/AppError";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroup } from "@storage/player/playersGetByGroup";
 
 type RouteParams = {
   group: string;
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('');
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Rodrigo', 'Diego', 'Ana', 'Biro', 'Mayk', 'Vini', 'Hudson', 'Amanda', 'Gustavo', 'João', 'Pedro']);
+  const [players, setPlayers] = useState([]);
   
   const route = useRoute();
   const { group } = route.params as RouteParams;
   
+  async function hanldeAddPlayer() {
+    if(newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar');
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    }
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+      const players = await playersGetByGroup(group);
+      console.log(players)
+    } catch (error) {
+      if(error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message);
+      } else {
+        console.log(error);
+        Alert.alert('Nova pessoa', 'Não foi possível adicionar');
+      }
+    }
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -32,10 +60,11 @@ export function Players() {
 
       <Form>
         <Input 
+          onChangeText={setNewPlayerName}
           placeholder="Nome da pessoa"
           autoCorrect={false}
         />
-        <ButtonIcon icon="add" />
+        <ButtonIcon onPress={hanldeAddPlayer} icon="add" />
       </Form>
       <HeaderList>
         <FlatList 
